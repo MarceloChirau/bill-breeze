24ee8c13b6da0b0e8d9b8cf0deb96058dceed2af
marceloDev <marcelochirau@gmail.com>
2026-04-07 17:49:34 +0200

    sign-google, create-user and login-user are ready

--- public/index.js
+++ public/index.js
@@ -1,11 +1,30 @@
-import { signInFunction, signOutFunction,auth } from "./firebaseConfig.js";
+import { signInFunction, signOutFunction,auth} from "./firebaseConfig.js";
 import {
-      onAuthStateChanged
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword
 
   } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
 
 const signInBtn=document.getElementById('signBtn');
 const signOutBtn=document.getElementById('signOutBtn');
+
+const signUpBtn=document.getElementById('signUp');
+const submitBtn=document.getElementById('createUserBtn');
+const emailInput=document.getElementById('email');
+const passwordInput=document.getElementById('password');
+
+const logAlreadyUserBtn=document.getElementById('logAlreadyUser');
+const alreadyEmailInput=document.getElementById('alreadyEmail');
+const alreadyPasswordInput=document.getElementById('alreadyPassword');
+const logUserBtn=document.getElementById('logUserBtn');
+const createUserContainer=document.querySelector('.createUserContainer');
+
+
+/*<button type="button" id="logAlreadyUser">log-user</button>
+<input type="text" placeholder="email.." id="alreadyEmail" hidden>
+<input type="password" placeholder="your password.." id="alreadyPassword" hidden>
+<button type="button" id="logUserBtn" hidden>submit user</button>*/
 
 
 if (!signInBtn) {
@@ -36,7 +55,11 @@ signOutBtn?.addEventListener('click',async()=>{
         if (ok===null){
             console.log('sign out failed');
         }else{
+            console.log('this is what ok has inside:',ok)
             console.log('log out succeded!')
+            console.log('showing the container with other options:')
+createUserContainer.hidden=false;
+
         }
     }catch(error){
         console.error("error:",error);
@@ -56,3 +79,66 @@ onAuthStateChanged(auth,(user)=>{
         signOutBtn.hidden=true; 
     }
 })
+
+
+signUpBtn?.addEventListener('click',()=>{
+    emailInput.hidden=false;
+    passwordInput.hidden=false;
+    submitBtn.hidden=false;
+})
+
+submitBtn?.addEventListener('click',async()=>{
+    try{
+        console.log('email:',emailInput.value);
+        console.log('password:',passwordInput.value);
+
+        const result=await createUserWithEmailAndPassword(auth,emailInput.value,passwordInput.value);
+        const user=result.user;
+        console.log('user is creared:',user)
+        console.log('user id:',user?.uid)
+        //making empty the inputs and hide the container:
+        emailInput.value="";
+        passwordInput.value=""
+createUserContainer.hidden=true;
+
+        return user;
+    }catch(error){
+        console.error('Couldnt create a user:',error)
+        console.log('msg:',error.message)
+    }
+})
+
+
+logAlreadyUserBtn?.addEventListener('click',()=>{
+    alreadyEmailInput.hidden=false;
+alreadyPasswordInput.hidden=false;
+logUserBtn.hidden=false;
+})
+
+
+
+logUserBtn?.addEventListener('click',async()=>{
+    try{
+
+        console.log('already a user, email:',alreadyEmailInput.value);
+        console.log('already a user, password:',alreadyPasswordInput.value);
+        const result=await signInWithEmailAndPassword(auth,alreadyEmailInput.value,alreadyPasswordInput.value);
+    
+        const user=result.user;
+        console.log('already an existant user:',user);
+        console.log('this user exists and his id is :',user.uid)
+//hiding everything except the signOut and making the inputs empty:
+alreadyEmailInput.value="";
+alreadyPasswordInput.value="";
+createUserContainer.hidden=true;
+
+        return user;
+    }catch(err){
+        console.error('there is no user with these credentials:',err.message);
+
+    }
+})
+
+//email: marcelochirau@gmail.com
+// password: 123swd3
+// uid: RQJ7kdSTmMW0AIFTcs2UMNPoMq63
